7a29ad6ee220409eb1b9e155f600ef3713821831
CCarrMcMahon <CCarrMcMahon@gmail.com>
2023-11-20 13:36:00 -0600

    Update configure.py with more simple import check

--- configure.py
+++ configure.py
@@ -1,14 +1,10 @@
-API_VERSION = '2.2.4'
-
 import sys
-try:
-    from ambuild2 import run
-    if not run.HasAPI(API_VERSION):
-        raise Exception()
-except:
-    sys.stderr.write('AMBuild {0} must be installed to build this project.\n'.format(API_VERSION))
-    sys.stderr.write('http://www.alliedmods.net/ambuild\n')
-    sys.exit(1)
+from ambuild2 import run
+
+
+API_VERSION = "2.2.4"
+if not run.HasAPI(API_VERSION):
+    raise ImportError(f"This plugin requires AMBuild version {API_VERSION} or similar to be installed.")
 
 builder = run.BuildParser(sourcePath=sys.path[0], api=API_VERSION)
 builder.options.add_argument(
