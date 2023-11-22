a92c8dbae6d94956f2076a6728c8e966482a0f5f
CCarrMcMahon <CCarrMcMahon@gmail.com>
2023-11-22 00:43:47 -0600

    Add a game name argument for manifest and sdk searching

--- configure.py
+++ configure.py
@@ -8,6 +8,7 @@ PLUGIN_NAME = "cs2-surf"
 PLUGIN_ALIAS = ""
 MMS_PATH = "metamod-source"
 HL2SDK_PATH = "hl2sdk-cs2"
+GAME_NAME = "cs2"
 
 
 if not run.HasAPI(API_VERSION):
@@ -15,13 +16,13 @@ if not run.HasAPI(API_VERSION):
 
 builder = run.BuildParser(sourcePath=sys.path[0], api=API_VERSION)
 builder.options.add_argument(
-    "--name",
+    "--plugin_name",
     type=str,
     default=PLUGIN_NAME,
     help="The overall name of the plugin.",
 )
 builder.options.add_argument(
-    "--alias",
+    "--plugin_alias",
     type=str,
     default=PLUGIN_ALIAS,
     help="An alias for the name of the plugin.",
@@ -36,6 +37,17 @@ builder.options.add_argument(
     "--hl2sdk_path",
     type=str,
     default=HL2SDK_PATH,
-    help="The path to the HL2SDK directory.",
+    help="""The path to the HL2SDK directory. If the path can't be found and --game_name is specified, then a path
+    with both joined together by a hyphen will be search for instead.
+    """,
+)
+builder.options.add_argument(
+    "--game_name",
+    type=str,
+    default=GAME_NAME,
+    help="""The name of the game this plugin is being built for. This is used when searching for a relevant manifest
+    file and can be empty if the path for the HL2SDK contains a hyphen. If this is specified and the HL2SDK path
+    contains a hyphen, this value will take priority for the game name.
+    """,
 )
 builder.Configure()
