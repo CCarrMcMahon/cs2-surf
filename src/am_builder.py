09efefaa83ecf6331c8214ec9c893826429f505d
CCarrMcMahon <CCarrMcMahon@gmail.com>
2023-11-26 18:08:55 -0600

    Use context instead of AMBuilder for plugin class and move source specification to am_builder.py file

--- src/am_builder.py
+++ src/am_builder.py
@@ -1,2 +1,3 @@
-
- self.get_path_from_root("src", "sample_mm.cpp"),
+am_builder = plugin.create_am_builder(builder, plugin.plugin_name, "Library")
+am_builder.add_binary_sources(["sample_mm.cpp"])
+plugin.add_am_builder(am_builder)
