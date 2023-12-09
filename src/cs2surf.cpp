8ea45db5910de1c7365dc5e6d7dd91a9798dda40
CCarrMcMahon <CCarrMcMahon@gmail.com>
2023-12-09 16:29:14 -0600

    Use cs2surf namespace

--- src/cs2surf.cpp
+++ src/cs2surf.cpp
@@ -1,39 +1,42 @@
 #include "cs2surf.h"
 
-CS2Surf g_CS2Surf;
+cs2surf::CS2Surf g_CS2Surf;
 
-PLUGIN_EXPOSE(CS2Surf, g_CS2Surf);
-bool CS2Surf::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late) {
+PLUGIN_EXPOSE(cs2surf::CS2Surf, g_CS2Surf);
+bool cs2surf::CS2Surf::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late) {
     PLUGIN_SAVEVARS();
 
     // Hook into IMetamodListener Events
     g_SMAPI->AddListener(this, this);
 
-    META_CONPRINTF("CS2Surf::Load() Called.\n");
+    META_CONPRINTF("cs2surf::CS2Surf::Load() Called.\n");
     return true;
 }
 
-bool CS2Surf::Unload(char *error, size_t maxlen) { return true; }
-
-void CS2Surf::OnLevelInit(char const *pMapName, char const *pMapEntities, char const *pOldLevel,
-                          char const *pLandmarkName, bool loadGame, bool background) {
-    META_CONPRINTF("CS2Surf::OnLevelInit() called with map \"%s\".\n", pMapName);
+bool cs2surf::CS2Surf::Unload(char *error, size_t maxlen) {
+    META_CONPRINTF("cs2surf::CS2Surf::Unload() Called.\n");
+    return true;
 }
 
-void CS2Surf::OnLevelShutdown() { META_CONPRINTF("CS2Surf::OnLevelShutdown() called.\n"); }
+void cs2surf::CS2Surf::OnLevelInit(char const *pMapName, char const *pMapEntities, char const *pOldLevel,
+                                   char const *pLandmarkName, bool loadGame, bool background) {
+    META_CONPRINTF("cs2surf::CS2Surf::OnLevelInit() called with map \"%s\".\n", pMapName);
+}
 
-const char *CS2Surf::GetAuthor() { return "CCarrMcMahon"; }
+void cs2surf::CS2Surf::OnLevelShutdown() { META_CONPRINTF("cs2surf::CS2Surf::OnLevelShutdown() called.\n"); }
 
-const char *CS2Surf::GetName() { return "cs2-surf"; }
+const char *cs2surf::CS2Surf::GetAuthor() { return "CCarrMcMahon"; }
 
-const char *CS2Surf::GetDescription() { return "An open source plugin for surfing in CS2."; }
+const char *cs2surf::CS2Surf::GetName() { return "cs2-surf"; }
 
-const char *CS2Surf::GetURL() { return "N/A"; }
+const char *cs2surf::CS2Surf::GetDescription() { return "An open source plugin for surfing in CS2."; }
 
-const char *CS2Surf::GetLicense() { return "GNU GENERAL PUBLIC LICENSE"; }
+const char *cs2surf::CS2Surf::GetURL() { return "N/A"; }
 
-const char *CS2Surf::GetVersion() { return "v0.0.1-alpha"; }
+const char *cs2surf::CS2Surf::GetLicense() { return "GNU GENERAL PUBLIC LICENSE"; }
 
-const char *CS2Surf::GetDate() { return __DATE__; }
+const char *cs2surf::CS2Surf::GetVersion() { return "v0.0.1-alpha"; }
 
-const char *CS2Surf::GetLogTag() { return "[cs2-surf]"; }
+const char *cs2surf::CS2Surf::GetDate() { return __DATE__; }
+
+const char *cs2surf::CS2Surf::GetLogTag() { return "[cs2-surf]"; }
--- src/cs2surf.h
+++ src/cs2surf.h
@@ -3,28 +3,30 @@
 
 #include <ISmmPlugin.h>
 
-class CS2Surf : public ISmmPlugin, public IMetamodListener {
-  public: // ISmmPlugin (States)
-    bool Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late) override;
-    bool Unload(char *error, size_t maxlen) override;
+namespace cs2surf {
+    class CS2Surf : public ISmmPlugin, public IMetamodListener {
+      public: // ISmmPlugin (States)
+        bool Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late) override;
+        bool Unload(char *error, size_t maxlen) override;
 
-  public: // ISmmPlugin (Information)
-    const char *GetAuthor() override;
-    const char *GetName() override;
-    const char *GetDescription() override;
-    const char *GetURL() override;
-    const char *GetLicense() override;
-    const char *GetVersion() override;
-    const char *GetDate() override;
-    const char *GetLogTag() override;
+      public: // ISmmPlugin (Information)
+        const char *GetAuthor() override;
+        const char *GetName() override;
+        const char *GetDescription() override;
+        const char *GetURL() override;
+        const char *GetLicense() override;
+        const char *GetVersion() override;
+        const char *GetDate() override;
+        const char *GetLogTag() override;
 
-  public: // IMetamodListener (Events)
-    void OnLevelInit(char const *pMapName, char const *pMapEntities, char const *pOldLevel, char const *pLandmarkName,
-                     bool loadGame, bool background) override;
-    void OnLevelShutdown() override;
-};
+      public: // IMetamodListener (Events)
+        void OnLevelInit(char const *pMapName, char const *pMapEntities, char const *pOldLevel,
+                         char const *pLandmarkName, bool loadGame, bool background) override;
+        void OnLevelShutdown() override;
+    };
+} // namespace cs2surf
 
-extern CS2Surf g_CS2Surf;
+extern cs2surf::CS2Surf g_CS2Surf;
 
 PLUGIN_GLOBALVARS();
 
